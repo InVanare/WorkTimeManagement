248d132865199a1a381da08e1b3c16c2efcdccb2
Paweł Adamski <invanare12@gmail.com>
2022-04-15 13:26:32 +0200

    SpringBoot run

---
+++ src/main/java/pl/coderslab/wtm/SpringBootApplicationRun.java
@@ -0,0 +1,13 @@
+package pl.coderslab.wtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationRun.class, args);
+    }
+
+}
